ccf0f86abc4f63c58bb336ca8fc1f52890f455b1
Tushar Sudake <etushar89@gmail.com>
2017-03-30 22:41:26 -0500

    Plotting chart

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -2,11 +2,74 @@
 var DataFrame = dfjs.DataFrame
 
 function visualize() {
+  document.getElementById('dataset-div').style.visibility = 'visible'
   document.getElementById('chartSelection').style.visibility = 'visible'
+  document.getElementById('chartDiv').style.visibility = 'visible'
+}
+
+function plotChart() {
+  document.getElementById('dataset-div').style.visibility = 'visible'
+  document.getElementById('chartSelection').style.visibility = 'visible'
+  document.getElementById('chartDiv').style.visibility = 'visible'
+
+  DataFrame.fromCSV('data/crdbt.csv').then(
+    df => {
+      //Chart
+      var selectedDf = df.select('day_type','total_rides')
+      var result = selectedDf.groupBy('day_type').aggregate(group => group.stat.sum('total_rides')).rename('DayType', 'TotalRides');
+
+      //console.log(result.select('aggregation').toArray())
+
+      var ctx = document.getElementById("chartArea");
+      //alert('ok')
+      var myChart = new Chart(ctx, {
+        type: 'bar',
+        options: {
+          legend: {
+            display: false
+          }
+        },
+        data: {
+          labels: result.select('day_type').toArray(),
+          datasets: [{
+            label: 'Total Rides',
+            data: result.select('aggregation').toArray(),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero:true,
+                steps: 10,
+                stepValue: result.stat.max('aggregation') / 10,
+                max: result.stat.max('aggregation') * 1.1
+              }
+            }]
+          }
+        }
+      });
+    }
+  )
 }
 
 function load() {
+
   document.getElementById('dataset-div').style.visibility = 'visible'
+  document.getElementById('chartSelection').style.visibility = 'hidden'
+  document.getElementById('chartDiv').style.visibility = 'hidden'
+
   DataFrame.fromCSV('data/crdbt.csv').then(
     df => {
       var myTableDiv = document.getElementById("dataset")
@@ -48,47 +111,6 @@ function load() {
         tableBody.appendChild(tr);
       }
       myTableDiv.appendChild(table)
-
-      //Chart
-      var selectedDf = df.select('day_type','total_rides')
-      var result = selectedDf.groupBy('day_type').aggregate(group => group.stat.sum('total_rides')).rename('DayType', 'TotalRides');
-
-      console.log(result.select('aggregation').toArray())
-
-      var ctx = document.getElementById("myChart");
-      var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: result.select('day_type').toArray(),
-          datasets: [{
-            label: 'Total Rides',
-            data: result.select('aggregation').toArray(),
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)'
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero:true,
-                steps: 10,
-                stepValue: result.stat.max('aggregation') / 10,
-                max: result.stat.max('aggregation') * 1.1
-              }
-            }]
-          }
-        }
-      });
     }
   )
 }
