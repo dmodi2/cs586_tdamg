9c88a79af9d8047160f2852cad015dd523b41161
Dhruvit-IIT <dhruvit@104.194.112.182>
2017-04-14 19:00:51 -0500

    title changes

--- js/main.js
+++ js/main.js
@@ -2,16 +2,12 @@ function visualize() {
   document.getElementById('dataset-div').style.visibility = 'visible'
   document.getElementById('chartSelection').style.visibility = 'visible'
   document.getElementById('chartDiv').style.visibility = 'visible'
-  console.log("in visualize()")
 }
 
 function loadDatasetMain(){
 
-  console.log("in loadDatasetMain() 1")
   var dashboard = new Dashboard()
   var selectDataset = document.getElementById('datasetSelect')
   var sd = selectDataset.options[selectDataset.selectedIndex].value;
-  console.log(sd)
   dashboard.loadDataset(sd)
-  console.log("in loadDatasetMain() 4")
 }
