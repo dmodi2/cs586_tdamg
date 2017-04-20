f10afb1cade9f50c4ab3d716570c8fffabe81e74
Dhruvit-IIT <dhruvit@104.194.112.182>
2017-04-20 15:16:20 -0500

    Bar, Line and dynamic chart menu (except pivot) done
    Bar, Line and dynamic chart menu (except pivot) done for 3 datasets.
    Some UI refinements. Chart selection dynamic navigation done. Pivot left

--- js/Bar.js
+++ js/Bar.js
@@ -16,15 +16,15 @@ class Bar extends Graph {
   var file
   if(selectDataset == 'ds1'){
     file = "data/crdbt.csv"
-  }else if(selectDataset == 'ds2'){
+  }/*else if(selectDataset == 'ds2'){
     file = "data/CTA_Ridership_Bus_Routes.csv"
   }else if(selectDataset == 'ds3'){
     file = "data/CTA_Ridership_L_Station.csv"
-  }else if(selectDataset == 'ds4'){
+  }*/else if(selectDataset == 'ds4'){
     file = "data/Average_Daily_Traffic_Counts.csv"
-  }else if(selectDataset == 'ds5'){
+  }/*else if(selectDataset == 'ds5'){
     file = "data/Red_Light_Camera_Violations.csv"
-  }else if(selectDataset == 'ds6'){
+  }*/else if(selectDataset == 'ds6'){
     file = "data/Divvy_Bicycle_Stations.csv"
   }else{
     console.log("No file selcted")
@@ -35,7 +35,7 @@ class Bar extends Graph {
       //Chart
       var selectedDf = df.select(x_axis, y_axis)
       var result = selectedDf.groupBy(x_axis).aggregate(group => group.stat.sum(y_axis))/*.rename(map[x_axis], map[y_axis]);*/;
-      var ctx = document.getElementById("chartArea");
+      var ctx = document.getElementById("chartArea").getContext("2d");
       var colorArray = [
         'rgba(255, 99, 134, 0.2)',
         'rgba(54, 162, 235, 0.2)',
