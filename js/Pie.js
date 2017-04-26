fe78d0ce8f0e4a2403e0467909b93bb68f11347b
Dhruvit-IIT <dhruvit@104.194.112.182>
2017-04-25 19:22:14 -0500

    Added Pie Chart and minor changes in html

--- js/Pie.js
+++ js/Pie.js
@@ -3,27 +3,25 @@ class Pie extends Graph{
 		super()
 	}
 	plotGraph(x_axis,y_axis,selectDataset){
-	
+
 	document.getElementById('chartSelection').style.visibility = 'visible'
 	document.getElementById('chartDiv').style.visibility = 'visible'
 
 	  var DataFrame = dfjs.DataFrame
 
 	  var file
-	  if(selectDataset == 'ds1'){
-		file = "data/crdbt.csv"
-	  }/*else if(selectDataset == 'ds2'){
-		file = "data/CTA_Ridership_Bus_Routes.csv"
+		if(selectDataset == 'ds1'){
+	    file = "data/crdbt.csv"
 	  }else if(selectDataset == 'ds3'){
-		file = "data/CTA_Ridership_L_Station.csv"
-	  }*/else if(selectDataset == 'ds4'){
-		file = "data/Average_Daily_Traffic_Counts.csv"
-	  }/*else if(selectDataset == 'ds5'){
-		file = "data/Red_Light_Camera_Violations.csv"
-	  }*/else if(selectDataset == 'ds6'){
-		file = "data/Divvy_Bicycle_Stations.csv"
+	    file = "data/CTA_Ridership_L_Station.csv"
+	  }else if(selectDataset == 'ds4'){
+	    file = "data/Average_Daily_Traffic_Counts.csv"
+	  }else if(selectDataset == 'ds5'){
+	    file = "data/Red_Light_Camera_Violations.csv"
+	  }else if(selectDataset == 'ds6'){
+	    file = "data/Divvy_Bicycle_Stations.csv"
 	  }else{
-		console.log("No file selcted")
+	    console.log("No file selcted")
 	  }
 
 	  DataFrame.fromCSV(file).then(
@@ -32,19 +30,19 @@ class Pie extends Graph{
       var selectedDf = df.select(x_axis, y_axis)
       var result = selectedDf.groupBy(x_axis).aggregate(group => group.stat.sum(y_axis))/*.rename(map[x_axis], map[y_axis]);*/;
       var ctx = document.getElementById("chartArea").getContext("2d");
-	  
-	  
+
+
 	  var colorArray = [
-        'rgba(255, 99, 134, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)'
+							"#FF6384",
+							"#36A2EB",
+							"#FFCE56"
       ]
       var borColorArray = [
         'rgba(255, 99, 134, 1)',
         'rgba(54, 162, 235, 1)',
         'rgba(255, 206, 86, 1)'
       ]
-	  
+
 	  var col_count = 0
       var backgroundColorArray = []
       var borderColorArray = []
@@ -56,7 +54,7 @@ class Pie extends Graph{
           col_count = 0
         }
       }
-	  
+
 	  var myPieChart = new Chart(ctx,{
     type: 'pie',
     data: {
