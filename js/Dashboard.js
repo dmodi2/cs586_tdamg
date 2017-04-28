ae27c34c363767b7f7c23dfdba8c018d310095e3
Dhruvit-IIT <dhruvit@104.194.112.182>
2017-04-28 01:01:31 -0500

    Advanced Chart  Option

--- js/Dashboard.js
+++ js/Dashboard.js
@@ -68,4 +68,91 @@ class Dashboard {
       }
      )
     }
+
+    getValueForColumns(x_axis, y_axis, selectDataset){
+
+      var DataFrame = dfjs.DataFrame
+      var file
+      if(selectDataset == 'ds1'){
+        file = "data/crdbt.csv"
+      }else if(selectDataset == 'ds3'){
+        file = "data/CTA_Ridership_L_Station.csv"
+      }else if(selectDataset == 'ds4'){
+        file = "data/Average_Daily_Traffic_Counts.csv"
+      }else if(selectDataset == 'ds5'){
+        file = "data/Red_Light_Camera_Violations.csv"
+      }else if(selectDataset == 'ds6'){
+        file = "data/Divvy_Bicycle_Stations.csv"
+      }else{
+        console.log("No file selcted")
+      }
+
+      var value_array = []
+      DataFrame.fromCSV(file).then(
+          df => {
+            var selectedDf = df.select(x_axis);
+            var result = selectedDf.distinct(x_axis)
+            var a = result.toArray()
+            console.log(result.toArray());
+        	  for (var i=0; i<a.length; i++)
+        	  {
+        			value_array[i] = a[i][0]
+            }
+            var adv_opt = document.getElementById("adv_opt_col")
+            var text = document.createTextNode('Select ' + map[x_axis])
+            var br = document.createElement("br")
+            //Create and append select list
+            var selectList = document.createElement("select")
+            selectList.multiple = "multiple"
+            selectList.className = "form-control"
+            selectList.id = "select_column_values"
+            adv_opt.appendChild(br)
+            adv_opt.appendChild(text)
+            adv_opt.appendChild(selectList)
+
+            //Create and append the options
+            for (var i = 0; i < value_array.length; i++) {
+              var option = document.createElement("option")
+              option.value = value_array[i]
+              option.text = value_array[i]
+              selectList.appendChild(option)
+            }
+
+            adv_opt.appendChild(br)
+            text = document.createTextNode('Select ' + map[y_axis] + ' value: ')
+            adv_opt.appendChild(text)
+            var col_value_box = document.createElement('input')
+            col_value_box.setAttribute('type', 'text')
+            col_value_box.setAttribute('class', 'col_value')
+            adv_opt.appendChild(col_value_box)
+
+            document.getElementById('adv_opt_col').style.visibility = 'visible'
+            text = document.createTextNode('Select condition: ')
+            adv_opt.appendChild(br)
+            adv_opt.appendChild(text)
+
+            //Create and append select list
+            var selectList = document.createElement("select")
+            selectList.className = "form-control"
+            selectList.id = "col_value"
+            adv_opt.appendChild(selectList)
+
+            var option1 = document.createElement("option")
+            option1.value = 'lt'
+            option1.text = 'Less Than'
+            selectList.appendChild(option1)
+
+            var option2 = document.createElement("option")
+            option2.value = 'eq'
+            option2.text = 'Equal to'
+            selectList.appendChild(option2)
+
+            var option3 = document.createElement("option")
+            option3.value = 'gt'
+            option3.text = 'Greater Than'
+            selectList.appendChild(option3)
+
+          }
+      )
+    }
 }
