ec3bb89445fe06114ca214c90a2c1d079b9e5596
Tushar Sudake <etushar89@gmail.com>
2017-04-28 10:46:30 -0500

    Filter functionality

--- js/Dataframe.js
+++ js/Dataframe.js
@@ -3,4 +3,36 @@ class Dataframe {
     this.columns = columns;
     this.data = data;
   }
+
+  static lte(lv, rv) {
+    return lv <= rv
+  }
+
+  static lt(lv, rv) {
+    return lv < rv
+  }
+
+  static gte(lv, rv) {
+    return lv >= rv
+  }
+
+  static gt(lv, rv) {
+    return lv > rv
+  }
+
+  static eq(lv, rv) {
+    return lv == rv
+  }
+
+  static neq(lv, rv) {
+    return lv != rv
+  }
+
+  static strIn(lv, rv) {
+    return rv.includes(lv)
+  }
+
+  static strNIn(lv, rv) {
+    return !rv.includes(lv)
+  }
 }
--- js/Pie.js
+++ js/Pie.js
@@ -4,7 +4,7 @@ class Pie extends Graph{
     super(file)
   }
 
-	plotGraph(x_axis,y_axis){
+	plotGraph(x_axis, y_axis, x_filter_val, xOp, y_filter_val, yOp, doFilter){
 
 	document.getElementById('chartSelection').style.visibility = 'visible'
 	document.getElementById('chartDiv').style.visibility = 'visible'
@@ -15,6 +15,12 @@ class Pie extends Graph{
     df => {
       //Chart
       var selectedDf = df.select(x_axis, y_axis)
+
+			if (doFilter) {
+				var xFDf = selectedDf.filter(row => xOp(row.get(x_axis), x_filter_val))
+				selectedDf = xFDf.filter(row => yOp(row.get(y_axis), y_filter_val))
+			}
+
       var result = selectedDf.groupBy(x_axis).aggregate(group => group.stat.sum(y_axis))/*.rename(map[x_axis], map[y_axis]);*/;
       var ctx = document.getElementById("chartArea").getContext("2d");
 
