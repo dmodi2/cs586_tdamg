c915d7edd31aa878a70c99086709af781168eec8
monika priyadarshini <monikapriyadarshini@monikas-MacBook-Pro.local>
2017-04-28 20:30:09 -0500

    stats bar graph  added

--- js/Graph.js
+++ js/Graph.js
@@ -7,5 +7,8 @@ class Graph{
   resetCanvas(){
     $('#myChart').remove(); // this is my <canvas> element
     $('#chartDiv').append('<div id="myChart" align="center" style ="overflow : scroll; height:100%; width:600px;"> <canvas id="chartArea"><canvas></div>');
+
+    $('#myStatshart').remove(); // this is my <canvas> element
+    $('#statsDiv').append('<div id="myStatshart" align="center" style ="overflow : scroll; height:100%; width:600px;"> <canvas id="statsChartArea"><canvas></div>');
   }
 }
